e008276b9d98e506e2c6a9571c630906ccab5293
Gonzako <al341522@uji.es>
2019-12-14 19:28:54 +0100

    Update characterMovement.cs

--- Assets/Scripts/characterMovement.cs
+++ Assets/Scripts/characterMovement.cs
@@ -4,33 +4,44 @@
  * Gonzako123@gmail.com
  *
  */
- 
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
  
+
+[RequireComponent(typeof(Collider2D))]
 public class characterMovement : MonoBehaviour
 {
     #region Public Fields
+
     #endregion
- 
+
     #region Private Fields
+    Collider2D collider;
+
     #endregion
 
     #region Public Methods
+
     #endregion
 
     #region Private Methods
+
     #endregion
 
 
     #if true
     #region Unity API
 
-    void Start()
+    void Awake()
     {
+        collider = GetComponent<Collider2D>();
     }
  
     void FixedUpdate()
     {
+        Physics2D.Raycast();
     }
 
     #endregion
